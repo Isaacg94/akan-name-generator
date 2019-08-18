e2ab49cee9a45c14b9505e3d6db975b70b01bc45
Isaacg94 <7248zack@gmail.com>
2019-08-18 20:09:40 +0300

    additional js script

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function getAkanName() {
+  var yearOfBirth = document.getElementById("year-input").value;
+  var monthOfBirth = Number(document.getElementById("month-input").value);
+  var dayOfBirth = Number(document.getElementById("day-input").value);
+
+  var genders = document.getElementsByName("gender");
+
+  function getGender () {
+    for (var gender of genders){
+      if (gender.checked){
+        return gender.value;
+      } else {
+        alert("I respect your right not to conform into the binary gender identities but you have to select a gender for this to work.")
+      }
+    }
+  }
