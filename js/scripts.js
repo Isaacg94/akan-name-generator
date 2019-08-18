49055b0e72b943b5ed80f093bf086587a5c94486
Isaacg94 <7248zack@gmail.com>
2019-08-18 20:13:00 +0300

    add js day validation function

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,20 @@ function getAkanName() {
         return true;
       }
     }
+    function dayValidator() {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+      if (dayOfBirth > 28 || dayOfBirth < 1) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    }else if (dayOfBirth < 1 || dayOfBirth > 31) {
+      return false;
+    } else {
+      return true;
+    }
+  }
