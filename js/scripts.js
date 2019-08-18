241225448d3d80307a1f721fe903adc35026d5d6
Isaacg94 <7248zack@gmail.com>
2019-08-18 20:16:03 +0300

    add array of akan names to js

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,10 @@ function getAkanName() {
   var dayValid = dayValidator();
 
   var dayOfTheWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7)
+
+  var maleAkanNames = [
+    "Kwasi!", "Kwadwo!", "Kwabena!", "Kwaku!", "Yaw!", "Kofi!", "Kwame!"
+  ];
+  var femaleAkanNames = [
+    "Akosua!", "Adwoa!", "Abenaa!", "Akua!", "Yaa!", "Afua!", "Ama!"
+  ];
